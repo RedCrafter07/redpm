fdfefbe0ad2e8f01c38190ad284f70b57bc03449
RedCrafter07 <github@redcrafter07.de>
2022-02-03 19:23:50 +0100

    Some command handling stuff

--- index.ts
+++ index.ts
@@ -6,5 +6,30 @@ import gradient from 'gradient-string';
 import chalkAnimation from 'chalk-animation';
 import figlet from 'figlet';
 import { createSpinner } from 'nanospinner';
+import { cwd, argv } from 'process';
+import { execSync } from 'child_process';
+import pm2send from './lib/utils/pm2send';
 
-createSpinner("Loading...").start();
+const args = argv.slice(2);
+
+const command = args[0];
+
+(async () => {
+	switch (command) {
+		case 'i':
+		case 'install':
+			console.log('Soon™️');
+			break;
+		case 'test':
+			console.log(chalk.yellow(figlet.textSync('Hello')));
+			break;
+		case 'u':
+		case 'utils':
+			switch (args[1]) {
+				case 'pm2send':
+					pm2send();
+					break;
+			}
+			break;
+	}
+})();
